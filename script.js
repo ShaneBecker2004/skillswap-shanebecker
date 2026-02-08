f0a3055634e5bcb2af5d9a880084d6ea6a2df3c8
ShaneBecker2004 <shanemkbecker.2004@gmail.com>
2026-02-08 15:27:13 -0800

    Work in progress

--- script.js
+++
@@ -1,9 +0,0 @@
-const cards = document.querySelectorAll(".card");
-
-  cards.forEach(card => {
-    card.addEventListener("click", () => {
-      const details = card.querySelector(".details");
-      details.classList.toggle("hidden");
-    });
-  });
-
